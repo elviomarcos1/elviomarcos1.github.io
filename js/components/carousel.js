a19817280868064ec6933f952189ff26239c7ead
Elvio Marcos <elviomarcosfj@gmail.com>
2025-01-05 14:23:08 -0300

    Fix button-arow

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -2,15 +2,14 @@ const buttonArrowRight = document.querySelector('.main-button.-about.-right');
 const buttonArrowLeft = document.querySelector('.main-button.-about.-left');
 const items = document.querySelector('.items');
 
-let pixel = 70;
+let pixel = 0;
 
-
-buttonArrowRight.addEventListener('click', function(){
-    pixel -= 70;
+buttonArrowLeft.addEventListener('click', function(){
+    pixel = pixel + 100;
     items.style = `transform: translateX(${pixel}px)`;
 });
 
-buttonArrowLeft.addEventListener('click', function(){
-    pixel += 70;
+buttonArrowRight.addEventListener('click', function(){
+    pixel = pixel - 100;
     items.style = `transform: translateX(${pixel}px)`;
 });
