4003539aa27e9acffbbd625e1daa4aa7217ee42c
Elvio Marcos <elviomarcosfj@gmail.com>
2025-01-05 04:59:51 -0300

    Create contacts section

--- js/components/carousel.js
+++ js/components/carousel.js
@@ -2,15 +2,15 @@ const buttonArrowRight = document.querySelector('.main-button.-about.-right');
 const buttonArrowLeft = document.querySelector('.main-button.-about.-left');
 const items = document.querySelector('.items');
 
-let pixel = 25;
+let pixel = 70;
 
 
 buttonArrowRight.addEventListener('click', function(){
-    pixel -= 25;
+    pixel -= 70;
     items.style = `transform: translateX(${pixel}px)`;
 });
 
 buttonArrowLeft.addEventListener('click', function(){
-    pixel += 25;
+    pixel += 70;
     items.style = `transform: translateX(${pixel}px)`;
 });
